c3c899dbaee84e118fa547700b810f3e5ce0a6f3
SenaCrasher <asrafael255@gmail.com>
2025-05-07 01:25:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Zuroku", "Suaminya", " Mbak", "Shofwaa"],
+    strings: ["Selamat Datang", "Di Beranda", "Website Resmi", "AlwaysZuroku","Ada Yang Bisa","Saya Bantu?"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
