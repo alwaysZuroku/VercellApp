9aa35ecff7c069ba57b00c8357f23043ce10019a
SenaCrasher <asrafael255@gmail.com>
2025-05-07 16:11:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".text", {
     strings: ["Halo!! It`sMe AlwaysZuroku : Pembahasan Tentang JavaScript Yang Di Gunakan Untuk Membuat Bot WhatsApp Dengan Jangka Waktu Yang Tidak Di Tentukan Pecinta Bahasa Pemrograman / Orang Yang Memahami Tentang Bahasa Pemrograman Akan Faham Tentang Beberapa Methode Dalam Ilmu Tersebut Contoh Saja : JavaScript, HTML, Css, Python Dll"],
-    typeSpeed: 1000,
+    typeSpeed: 100,
     backSpeed: 1000,
     backDelay: 1000,
     loop: true
