ba389cf4c3b72eb9312ed8ffe33c2a9129acec63
SenaCrasher <asrafael255@gmail.com>
2025-05-07 14:51:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Selamat Datang", "Di Beranda", "Website Resmi", "AlwaysZuroku","Saya Hanya Ingin"," Mengungkapkan bahwa ","Tidak Semua Orang Yang","Kau Anggap Baik Itu","Benar² Baik","Akan Ada Masanya","Kau Tersadar Bahwa Orang-orang","Yang Kau Anggap Buruk","Ternyata Adalah Orang²","Terbaik Yang Pernah","Kau Temukan Di Dunia Ini","😇😇"]
+    strings: ["Selamat Datang", "Di Beranda", "Website Resmi", "AlwaysZuroku","Saya Hanya Ingin"," Mengungkapkan bahwa ","Tidak Semua Orang Yang","Kau Anggap Baik Itu","Benar² Baik","Akan Ada Masanya","Kau Tersadar Bahwa Orang-orang","Yang Kau Anggap Buruk","Ternyata Adalah Orang²","Terbaik Yang Pernah","Kau Temukan Di Dunia Ini","😇😇"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
